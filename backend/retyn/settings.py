fe1df30334d19a4e368d3bcd621614daa5c82aca
Agney <agney.saidas7@gmail.com~>
2026-04-05 12:19:08 +0530

    add pythondotenv

--- backend/retyn/settings.py
+++ backend/retyn/settings.py
@@ -14,9 +14,7 @@ from pathlib import Path
 from celery.schedules import crontab
 import sentry_sdk
 import os
-from dotenv import load_dotenv
 
-load_dotenv(".env.dev")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
